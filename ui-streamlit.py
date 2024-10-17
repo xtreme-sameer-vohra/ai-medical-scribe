325d06eaef6072b2e37084cf07f4a95a475efb8d
Sameer Vohra <vohra.sam@gmail.com>
2024-10-17 18:40:47 -0400

    connect ui app with local ollama mistral llm to generate soap note

--- ui-streamlit.py
+++ ui-streamlit.py
@@ -3,10 +3,18 @@ import whisper
 import numpy as np
 from scipy.io import wavfile
 from scipy.signal import resample
+import requests
+
+st.title("Medical AI Scribe")
 
 audio_value = st.experimental_audio_input("Record note to transcribe")
 
 stt = whisper.load_model("base.en")
+
+prompt = "Write the following transcribed text into SOAP(Subjective, Objective, Assessment, and Plan) format. Use full sentences rather than bullet points under each section. Transcribed text:"
+
+if 'transcribedtext' not in st.session_state:
+    st.session_state.transcribedtext = ''
 
 if audio_value:
     st.audio(audio_value)
@@ -23,4 +31,30 @@ if audio_value:
     result = stt.transcribe(audio_np, fp16=False)
     text = result["text"].strip()
     print("transcription complete :", text )
-    st.write(text)
+    st.session_state.transcribedtext = text
+
+if 'transcribedtext' in st.session_state and len(st.session_state.transcribedtext) > 0:
+    txt = st.text_area("Transcribed Text:", st.session_state.transcribedtext)
+    if st.button("Generating SOAP Note", key="soapnote"):
+        print("Generating SOAP Note" )
+        st.write("Generating SOAP Note")
+        # Send the transcribed text to Ollama LLM
+        api_endpoint = "http://localhost:11434/api/generate"
+        headers = {
+            "Content-Type": "application/json",
+        }
+        payload = {
+            "model": "mistral",
+            "prompt": prompt + " " + text,
+            "stream": False,
+        }
+
+        response = requests.post(api_endpoint, headers=headers, json=payload)
+
+        if response.status_code == 200:
+            response_data = response.json()
+            print("LLM response:", response_data)
+            st.write(response_data['response'])
+        else:
+            print("Failed to get response from LLM:", response.status_code)
+            st.write("Failed to get response from LLM:", response.status_code)
